b60d1903c003a25fcd6c20ae682f4b4f98d28ca0
niroshasistu <sistunirosha243@gmail.com>
2026-01-06 12:11:33 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let marks=100
+
+if (marks>=90){
+    console.log("your grade is A");
+}
+else if (marks>=75){
+    console.log("your grade is B");
+}
+else if (marks>=60){
+     console.log("your grade is C");
+}
+else {
+    console.log("fail");
+}
