bd04882c675844ffa90e40449cb3b6d208efcb81
niroshasistu <sistunirosha243@gmail.com>
2026-01-06 12:37:35 +0530

    Add files via upload

---
+++ switchcase.js
@@ -0,0 +1,34 @@
+let day=2
+switch(day)
+{
+    case 1:
+    if(day==1)
+      console.log("monday");
+    break;
+    case 2:
+    if(day==2)
+      console.log("tuesday");
+    break;
+    case 3:
+    if(day==3)
+      console.log("Wednesday");
+    break;
+    case 4:
+    if(day==4)
+      console.log("thursday");
+    break;
+    case 5:
+    if(day==5)
+      console.log("Friday");
+    break;
+      case 6:
+    if(day==6)
+      console.log("saturday");
+    break;
+      case 7:
+    if(day==7)
+      console.log("sunday");
+    break;
+    default:
+    console.log("Have a nice day")
+}
