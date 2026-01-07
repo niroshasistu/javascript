659d95bda0d7857f68910e3e4ccf36302b7cbee9
niroshasistu <sistunirosha243@gmail.com>
2026-01-07 12:11:02 +0530

    Add files via upload

---
+++ pattern.js
@@ -0,0 +1,35 @@
+// let n = 5
+// let row = ""
+
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+//     row+="\n";
+// }
+// console.log(row);
+// let n = 5;
+// let row = "";
+
+// for (let i = n; i >= 1; i--) {
+
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+//     row += "\n";
+// }
+// console.log(row);
+// let name = "Nirosha";
+// let row = "";
+
+// for (let i = 1; i <= name.length; i++) {
+
+//     for (let space = 1; space <= name.length - i; space++) {
+//         row += "";
+//     }
+//     for (let j = 1; j <= 2 * i; j++) {
+//         row += name[i-1] + "";
+//     }
+//     row += "\n";
+// }
+// console.log(row);
