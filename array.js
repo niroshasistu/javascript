fc470846040aa9416f8a7a21ddb9a6a982de1f7b
niroshasistu <sistunirosha243@gmail.com>
2026-01-08 11:51:42 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+function demo() {
+    let arr = [name = "niroshasistu",
+    age = 22,
+    course = "frontenddeveloper"
+    ];
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+demo();
+// output
+// niroshasistu
+// 22
+// frontenddeveloperR
