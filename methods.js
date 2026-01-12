5c1b1f002e5109f2e7b4306ee52ef8b100c792c6
niroshasistu <sistunirosha243@gmail.com>
2026-01-12 12:55:01 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,82 @@
+// function sum() {
+//     let number = [100, 200];
+//     let total = 0;
+//     for (let i = 0; i < number.length; i++) {
+//         total +=number[i];
+//     }
+//     return total;
+// }
+// console.log("sum of elements is:", sum());
+// output
+// sum of elements is: 300
+// string methods
+// function ConvertToUpperCase(str){
+//     return str.toUpperCase();
+// }
+// console.log("Uppercase string is:",ConvertToUpperCase("nirosha"));
+// output
+// Uppercase string is: NIROSHA
+// lowercase
+// function ConvertToLowerCase(str){
+//     return str.toLowerCase();
+// }
+// console.log("Lowercase string is:",ConvertToLowerCase("DEVELOPER"));
+// output
+// Lowercase string is: developer
+// slice 
+// function color(){
+//  let colors=["red","green","blue","yellow"];
+// let result =colors.slice(-2);
+// return result;
+// }
+//  console.log(color());
+// output
+// [ 'blue', 'yellow' ]
+// include:
+// function pink(str,substr){
+//     return str.includes(substr);
+// }
+// console.log(pink("sistu nirosha","sistu"));
+// console.log(pink("sistu nirosha","Sistu"));
+// output;
+// true
+// false
+// array methods
+// function pusharray() {
+//     let fruits = ["mango", "apple", "banana"];
+//     fruits.push("papaya");
+//     return fruits;
+// }
+// console.log(pusharray());
+// outputs:
+// [ 'mango', 'apple', 'banana', 'papaya' ]
+// function poparray() {
+//     let fruits = ["mango", "apple", "banana"];
+//     fruits.pop("apple");
+//     return fruits;
+// }
+// console.log(poparray());
+// output
+// [ 'mango', 'apple' ]
+// maparray
+// function maparray() {
+//     let numbers = [1, 2, 3, 4];
+//     let result = numbers.map(function (num) {
+//         return num * 2;
+//     });
+//     return result;
+// }
+// console.log(maparray());
+// output:
+// [ 2, 4, 6, 8 ]
+// filterarray
+// function filterarray() {
+//     let numbers = [10, 20, 30, 40];
+//     let result = numbers.filter(function (num) {
+//         return num >25;
+//     });
+//     return result;
+// }
+// console.log(filterarray());
+// output:
+// [ 30, 40 ]
