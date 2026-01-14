03897bb2d517f26cfa572a26915758410da9a6e2
niroshasistu <sistunirosha243@gmail.com>
2026-01-14 09:59:55 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,14 @@
+// // with parameter callback function
+
+// function calculate(a,b,callback){
+// let result=a+b;
+// callback(result);
+// }
+// function displayResult(res){
+//     console.log("result:",res);
+// }
+// calculate(10,20,displayResult);
+
+// anonymous function
+ setTimeout(()=>{
+    console.log("this  is nirosha after 2 seconds");},2000);
